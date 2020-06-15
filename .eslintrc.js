1774de733fa9ce6ed533d39ed8345b5fa307beb0
rickyshin <rihua.chen@zatech.com>
2020-06-15 22:52:52 +0800

    fix exlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
   extends: [require.resolve('@umijs/fabric/dist/eslint')],
-  'multiline-ternary': never,
-
+  rules: {
+    // "import/no-extraneous-dependencies": ["error", {"packageDir": ''}]
+    'import/no-extraneous-dependencies': [
+      0,
+      { devDependencies: true, optionalDependencies: true, peerDependencies: true },
+    ],
+  },
   globals: {
+    // ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
     REACT_APP_ENV: true,
   },
--- src/global.tsx
+++ src/global.tsx
@@ -1,8 +1,8 @@
 import { Button, message, notification } from 'antd';
-
 import React from 'react';
 import { formatMessage } from 'umi';
 import defaultSettings from '../config/defaultSettings';
+
 const { pwa } = defaultSettings;
 // if pwa is true
 if (pwa) {
